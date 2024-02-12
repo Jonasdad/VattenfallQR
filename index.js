32792a0db5564cee6df8e2a1889c89edd9ba1e8f
Jonas Dåderman <Jonasdad@kth.se>
2024-02-12 16:06:56 +0100

    Added more queries to queries.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const http = require('http')
 const  pool  = require('./db/dbConfig.js');
-const { getDatumBySN, getMostFrequentSN, getkwhBySnAndDate, getDataFromSNAndDate } = require('./db/queries.js');
+const { getDatumBySN, getMostFrequentSN, getkwhBySnAndDate, getDataFromSNAndDate, getkwhAndDateTimeBySn } = require('./db/queries.js');
 
 //const {parseCSV} = require('./data')
 
@@ -13,8 +13,12 @@ const app = express()
 
 app.use(express.static('public'))
 
-const data = getkwhBySnAndDate(pool, '07/12/2022', '009074');
-console.log(data);
+const runQueries = async () => {
+    const  data = await getkwhAndDateTimeBySn(pool, ['009074']);
+    console.log(data);
+
+}
+runQueries();
 console.log("Listening on port 8080:");
 const httpServer= http.createServer(app);
 httpServer.listen(8080);
